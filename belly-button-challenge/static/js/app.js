ca5159de63b3a131bed5b3a01c725bdc4841e055
doran73 <124732500+doran73@users.noreply.github.com>
2023-06-05 20:08:52 -0400

    code update
    submitted what I done so far

--- belly-button-challenge/static/js/app.js
+++ belly-button-challenge/static/js/app.js
@@ -1,88 +1,101 @@
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
-    let sampleNames = data.names;
-    let sampleMetadata = data.metadata; 
-    let samples = data.samples;
-    let sampleNumber = 940
-    let resultsArray = samples.filter(sampleObj => sampleObj.id == sampleNumber)
+let samples_display = [];
+let samples_meta = [];
+let samples_name = [];
+let samples_results = [];
 
-    let firstResult = resultsArray[0]
+const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
+let d3JSON = d3.json(url)
+let d3Output = d3.json(url).then(function(data){
+       let sampleNames = data.names;
+       let sampleMetadata = data.metadata; 
+       let samples = data.samples;
+       let selector = d3.select("#selDataset");
+       samples_display.push(samples)
+       //samples_meta.push(sampleMetadata)
+       let resultsArray = samples.filter(sampleObj => sampleObj.id);
+       let metaArray = sampleMetadata.filter(obj => obj.id);
 
-    let otu_labels = firstResult.otu_labels
-    let otu_ids = firstResult.otu_ids; 
-    let sample_values = firstResult.sample_values;
+       
+       
+       
+       
 
+       
+       console.log(metaArray); 
+       //get the first element of the array
+       for (let i = 0; i < resultsArray.length; i++) {
+        let otu_labels = resultsArray[i].otu_labels
+        let otu_ids = resultsArray[i].otu_ids; 
+        let value_samples = resultsArray[i].sample_values;
+        selector.append("option").text(resultsArray[i].id);
+        selector.append("option").property("value",resultsArray[i].id);
+        samples_meta.push(metaArray[i]);
+        samples_results.push(resultsArray[i]);
+   
+        //console.log(resultsArray[i].id)
+        //console.log(value_samples)
+       
     let barData = [
-        {
-            x:sample_values.slice(0,10).reverse(),
-            y:otu_ids.slice(0,10).reverse() ,
-            text: otu_labels.slice(0,10),
-            type: 'bar',
-            orientation : 'h'
-            
-        }
-    ]
+      {
+          x:value_samples.slice(0,10).reverse(),
+          y:otu_ids.slice(0,10).reverse() ,
+          text: otu_labels.slice(0,10),
+          type: 'bar',
+          orientation : 'h'
+          
+      }
+  ]
 
-    let barLayout = {
-        title: "Bar Graph",
+  let barLayout = {
+      title: "Bar Graph",
+  }
+
+  Plotly.newPlot("bar", barData, barLayout)
+          }
+        
+      
+  
+    
+ });
+
+ function optionChanged(result) {
+    let add_demo = [];
+  console.log(result)
+  for (let i = 0; i < samples_meta.length; i++) {
+
+   // console.log(samples_meta[i].age);
+
+   if (samples_meta[i].id == result)
+    {
+     // console.log(samples_meta[i].age);
+     //var my_name = 'John';
+     // var s = `hello ${my_name}, how are you doing`;
+
+     let demo_id = `ID: ${samples_meta[i].id}`;
+     add_demo.push(demo_id);
+     let demo_age = `Age: ${samples_meta[i].age}`;
+     add_demo.push(demo_age);
+     let demo_ethnic = `Ethnicity: ${samples_meta[i].ethnicity}`;
+     add_demo.push(demo_ethnic);
+     let demo_gender = `Gender: ${samples_meta[i].gender}`;
+     add_demo.push(demo_gender);
+     let demo_location = `Location: ${samples_meta[i].location}`;
+     add_demo.push(demo_location);
+     let demo_bbtype = `BBType ${samples_meta[i].bbtype}`;
+     add_demo.push(demo_bbtype);
+     let demo_wfreq = `Wfreg: ${samples_meta[i].wfreq}`;
+     add_demo.push(demo_wfreq);
+     //<h3 class="panel-title">Demographic Info</h3>
+     let d3Demo = d3.select("#output");
+     d3Demo.text(add_demo);
+     //send ID to function to populate bar chart based on ID selected
+     
+
+    
+     
+
+
     }
 
-    Plotly.newPlot("bar", barData, barLayout)
-
-    var bubbleData = [{
-        x: otu_ids,
-        y: sample_values,
-        mode: 'markers',
-        marker: {
-          color: otu_ids,
-          size: sample_values
-        }
-      }];
-            
-      var bubbleLayout = {
-        title: 'Bubble Chart',
-        xaxis: {title: "OTU IDs"},
-        height: 600,
-        width: 600
-      };
-      
-      Plotly.newPlot('bubble', bubbleData, bubbleLayout);
-
-      var gaugeData = [
-        {
-        x: [0],
-        y: [0],
-          type: "scatter",
-        },{
-          values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
-          title: { text: "Speed", font: { size: 24 } },
-          delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
-          gauge: {
-            axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
-            bar: { color: "darkblue" },
-            bgcolor: "white",
-            borderwidth: 2,
-            bordercolor: "gray",
-            steps: [
-              { range: [0, 250], color: "cyan" },
-              { range: [250, 400], color: "royalblue" }
-            ],
-            threshold: {
-              line: { color: "red", width: 4 },
-              thickness: 0.75,
-              value: 490
-            }
-          }
-        }
-      ];
-      
-      var gaugeLayout = {
-        width: 500,
-        height: 400,
-        margin: { t: 25, r: 25, l: 25, b: 25 },
-        paper_bgcolor: "lavender",
-        font: { color: "darkblue", family: "Arial" }
-      };
-      
-      Plotly.newPlot('gauge', gaugeData, gaugeLayout);
-
-})
+  }
+ };
